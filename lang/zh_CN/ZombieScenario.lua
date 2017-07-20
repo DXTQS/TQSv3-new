abf649247dd732b131acc00cb26e9e440a4c9150
duongkhatx <huynhduongkha@gmail.com>
2017-07-21 02:03:11 +0700

    up

--- lang/zh_CN/ZombieScenario.lua
+++ lang/zh_CN/ZombieScenario.lua
@@ -1,18 +1,18 @@
 
 return {
-	["zombie_mode"] = "僵尸模式",
-	["Zombie"] = "僵尸模式",
-	["zombie"] = "僵尸",
-	["die"] = "屍",
-	["xunmeng"] = "迅猛",
-	["#Xunmeng"] = "%from 的【迅猛】效果被触发，伤害从 %arg 点上升至 %arg2 点",
-	[":xunmeng"] ="<b>锁定技</b>，你的杀造成的伤害+1。你的杀造成伤害时若你体力大于1，你流失1点体力。",
-	["peaching"] = "互助",
+	["zombie_mode"] = "Chế độ Zombie", --僵尸模式
+	["Zombie"] = "Chế độ Zombie", --僵尸模式
+	["zombie"] = "Zombie", --僵尸
+	["die"] = "Tử Thi", --屍
+	["xunmeng"] = "Tấn Mãnh", --迅猛
+	["#Xunmeng"] = "%from đã phát động <b>【Tấn Mãnh】</b>，Sát thương từ %arg điểm, tăng lên thành %arg2 điểm.",
+	[":xunmeng"] ="Tỏa Định Kĩ，你的杀造成的伤害+1。你的杀造成伤害时若你体力大于1，你流失1点体力。",
+	["peaching"] = "Hỗ Trợ", --互助
 	[":peaching"] = "弃置1张桃，视为对一名距离1以内的玩家使用了青囊。",
-	["ganran"] = "感染",
-	[":ganran"] = "<b>锁定技</b>，你的装备牌都视为铁锁连环。",
-	["zaibian"] = "灾变",
-	[":zaibian"] = "<b>锁定技</b>，你的出牌阶段开始时，若人类玩家数-僵尸玩家数+1大于0，你多摸该数目的牌。",
+	["ganran"] = "Cảm Nhiễm", --感染
+	[":ganran"] = "Tỏa Định Kĩ，Thẻ trang bị của bạn được xem như xích.", --你的装备牌都视为铁锁连环。
+	["zaibian"] = "Tai Biến", --灾变
+	[":zaibian"] = "Tỏa Định Kĩ，你的出牌阶段开始时，若人类玩家数-僵尸玩家数+1大于0，你多摸该数目的牌。", --<b>锁定技</b>，你的出牌阶段开始时，若人类玩家数-僵尸玩家数+1大于0，你多摸该数目的牌。
 	["#ZaibianGood"] = "%from 的【%arg2】效果被触发，%from 获得了 %arg 张手牌",
 	["#Zombify"] = "%from 变成僵尸复活了！%from 失去理智并开始攻击人类！",
 	["@round"] = "退治",
